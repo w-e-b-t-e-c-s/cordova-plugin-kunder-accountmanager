8e5e37e9c3aa2200e48ab6255c1b379667a4c09a
patrick <webtecs@free.fr>
2022-08-29 22:12:49 +0200

    Version 1.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# 1.0.3
+## Description
+- Add function to remove only user and password for ios keychain
+
+# 1.0.2
+## Description
+- Added package.json file
+- Compatibility with new Cordova path files
+- Removed hooks
+
+# 1.0.1
+## Description
+- iOS bug fixed 
+- License change to MIT.
+- Readme updated.
+
+### Bug FIxes
+#### iOS
+- `addAccount` function plugin result returns error when insert and update operation fails.   
+
+# 1.0.0
+## Description
+First release of plugin
---
+++ www/AccountManager.js
@@ -0,0 +1,62 @@
+/*global module, cordova */
+'use strict';
+module.exports = (function() {
+
+  var _initWithKey = function(encryptionKey, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'initWithKey', [encryptionKey]);
+  };
+
+  var _addAccount = function(userName, password, accountType, group, userData, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'addAccount', [userName, password, accountType, group, userData]);
+  };
+
+  // For iOS remove all data from keychain. For Android remove all account
+  var _removeAccount = function(accountType, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'removeAccount', [accountType]);
+  };
+
+  // For iOS only remove user account and password. For Android remove all account
+  var _removeUserPassword = function(accountType, group, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'removeUserPassword', [accountType, group]);
+  };
+
+  var _getUserAccount = function(accountType, group, returnKey, successCallback, errorCallback){
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'getUserAccount', [accountType, group, returnKey]);
+  };
+
+  var _getPassword = function(accountType, group, key, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'getPassword', [accountType, group, key]);
+  };
+
+  var _getDataFromKey = function(accountType, group, key, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'getDataFromKey', [accountType, group, key]);
+  };
+
+  var _setUserData = function(accountType, group, data, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'setUserData', [accountType, group, data]);
+  };
+
+  var _changePassword = function(accountType, group, newPassword, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'changePassword', [accountType, group, newPassword]);
+  };
+
+  var _changePasswordToCero = function(accountType, group, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'changePassword', [accountType, group, "0000"]);
+  };
+
+
+
+  return {
+    initWithKey: _initWithKey,
+    registerAccount: _addAccount,
+    removeAccount: _removeAccount,
+    removeUserPassword: _removeUserPassword,
+    getUserAccount: _getUserAccount,
+    getPassword: _getPassword,
+    getDataFromKey: _getDataFromKey,
+    setUserData: _setUserData,
+    setPassword: _changePassword,
+    resetPassword: _changePasswordToCero,
+  };
+
+})();
